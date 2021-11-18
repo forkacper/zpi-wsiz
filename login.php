31ad12b70b1769b310cff9cbb92e7e65e2684f26
forkacper <kacperformas@gmail.com>
2021-11-18 19:38:31 +0100

    update strony logowania, dodanie pliku do wylogowyania

--- login.php
+++ login.php
@@ -5,7 +5,7 @@ require_once(TEMPLATES . '/header.php');
 
 ?>
 
-    <div class="login-header box-shadow">
+ <!--   <div class="login-header box-shadow">
         <div class="container-fluid d-flex justify-content-between align-items-center">
             <div class="brand-logo">
                 <a href="login.php">
@@ -13,7 +13,7 @@ require_once(TEMPLATES . '/header.php');
                 </a>
             </div>
         </div>
-    </div>
+    </div>-->
     <div class="login-wrap d-flex align-items-center flex-wrap justify-content-center">
         <div class="container">
             <div class="row align-items-center">

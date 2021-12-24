69f62ca9fce1fbd60455bfcd568db8d407ed4062
Kacper Formas <kacper.formas@ageno.pl>
2021-12-24 16:42:06 +0100

    refactor

--- index.php
+++
@@ -1,9 +0,0 @@
-<?php
-require('config.php');
-require(SRC . '/login/auth.php');
-
-require_once(TEMPLATES . '/header.php');
-require_once(TEMPLATES . '/navbar.php');
-require_once(TEMPLATES . '/left-panel.php');
-
-require_once(TEMPLATES . '/footer.php');

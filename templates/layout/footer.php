ef94792266bf2a70b48438f1b95bcd0926406abc
Kacper Formas <kacper.formas@ageno.pl>
2021-12-25 11:47:59 +0100

    refactor

---
+++ templates/layout/footer.php
@@ -0,0 +1,19 @@
+<div class="footer-wrap pd-20 mb-20 card-box">
+    TransZPI - Aplikacja do zarządzania transportami
+</div>
+</div>
+</div>
+
+<!-- js -->
+<script src="../../public/scripts/core.js"></script>
+<script src="../../public/scripts/script.min.js"></script>
+<script src="../../public/scripts/process.js"></script>
+<script src="../../public/scripts/layout-settings.js"></script>
+<script src="../../public/plugins/apexcharts/apexcharts.min.js"></script>
+<script src="../../public/plugins/datatables/js/jquery.dataTables.min.js"></script>
+<script src="../../public/plugins/datatables/js/dataTables.bootstrap4.min.js"></script>
+<script src="../../public/plugins/datatables/js/dataTables.responsive.min.js"></script>
+<script src="../../public/plugins/datatables/js/responsive.bootstrap4.min.js"></script>
+<script src="../../public/scripts/dashboard.js"></script>
+</body>
+</html>

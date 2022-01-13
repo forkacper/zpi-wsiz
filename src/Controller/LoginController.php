c0b8f1aa10d6ac99bb08c3127a70e3d40de14163
forkacper <kacperformas@gmail.com>
2022-01-14 00:41:20 +0100

    Added abstract model class, wip login controller

--- src/Controller/LoginController.php
+++ src/Controller/LoginController.php
@@ -5,6 +5,15 @@ namespace App\Controller;
 class LoginController extends AbstractController
 {
     public function loginAction() {
+
+        if($this->request->hasPost()) {
+            $loginData = [
+                'username' => $this->request->postParam('username'),
+                'password' => $this->request->postParam('password')
+            ];
+
+        }
+
         $this->view->render('login');
     }
 }

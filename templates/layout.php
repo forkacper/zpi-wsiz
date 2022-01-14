2fb4ec00c36076a93482b5da8974d0619ddeab81
forkacper <kacperformas@gmail.com>
2022-01-15 00:41:06 +0100

    Added messages to layout

--- templates/layout.php
+++ templates/layout.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 if($page === 'login') {
     require_once ('layout/head.php');
     require_once ("templates/pages/$page.php");
+    require_once ("layout/messages.php");
     require_once ('layout/footer.php');
 } else {
     require_once ('layout/head.php');
@@ -11,6 +12,7 @@ if($page === 'login') {
     require_once ('layout/left-panel.php');
     !($page == 'dashboard') ? require_once('layout/breadcrumb.php') : '';
     require_once ("templates/pages/$page.php");
+    require_once ("layout/messages.php");
     require_once ('layout/footer.php');
 }
 

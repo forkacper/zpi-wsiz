8af2b1ec9e61826c3b56d8844dd9f990594a7fd1
forkacper <kacperformas@gmail.com>
2022-01-15 01:39:09 +0100

    Fixed messages

--- templates/layout/messages.php
+++ templates/layout/messages.php
@@ -1,25 +1,19 @@
 <?php
-if(!empty($params['error'])):
-   foreach ($params as $param): ?>
 
-       <div class="alert alert-warning alert-dismissible fade show" role="alert" style="$">
-           <strong>Uwaga!</strong> <?= $param ?>
-           <button type="button" class="close" data-dismiss="alert" aria-label="Close">
-               <span aria-hidden="true">×</span>
-           </button>
-       </div>
-
-<?php
-   endforeach; endif;
-
-if(!empty($params['success'])):
-    foreach ($params as $param): ?>
-
-        <div class="alert alert-success alert-dismissible fade show" role="alert">
-            <strong>Sukces</strong> <?= $param ?>
+foreach ($params as $param):
+    if(!empty($param['error'])): ?>
+        <div class="alert alert-warning alert-dismissible fade show" role="alert" style="">
+            <strong>Uwaga!</strong> <?= $param['error'] ?>
             <button type="button" class="close" data-dismiss="alert" aria-label="Close">
                 <span aria-hidden="true">×</span>
             </button>
         </div>
-
-<?php endforeach;endif; ?>
+    <?php endif;
+    if(!empty($param['success'])): ?>
+        <div class="alert alert-success alert-dismissible fade show" role="alert">
+            <strong>Sukces</strong> <?= $param['success'] ?>
+            <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+                <span aria-hidden="true">×</span>
+            </button>
+        </div>
+    <?php endif; endforeach;?>
